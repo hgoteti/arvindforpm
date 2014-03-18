f4f1cfec2a0a469138fb5ea2a8fcbb1fa5aac53e
Harsh Jain <harsh@crispygam.es>
2014-03-18 11:01:38 +0530

    boom

--- js/loksabha.js
+++ js/loksabha.js
@@ -1,12 +1,15 @@
 var loksabhaApp = angular.module('loksabhaApp', []);
  
+var GLOBAL_CANDIDATES = [
+			 {'name' : 'Ashutosh', 'snippet' : 'Lorem Ipsum Lorem Ipsum Lorem Ipsum', 'photo' : 'img/ashutosh.jpg','occupation': 'journalist', 'education' : 'btech', 'constituency' : 'Delhi North', 'state' : 'Delhi'},
+ 			 {'name' : 'Hemant Goteti', 'snippet' : 'Lorem Ipsum Lorem Ipsum Lorem Ipsum', 'photo' : 'img/ashutosh.jpg','occupation': 'journalist', 'education' : 'btech', 'constituency' : 'Delhi North', 'state' : 'Delhi'},
+			 {'name' : 'Harsh Jain', 'snippet' : 'Lorem Ipsum Lorem Ipsum Lorem Ipsum', 'photo' : 'img/ashutosh.jpg','occupation': 'journalist', 'education' : 'btech', 'constituency' : 'Delhi North', 'state' : 'Delhi'},
+			 {'name' : 'Ashutosh', 'snippet' : 'Lorem Ipsum Lorem Ipsum Lorem Ipsum', 'photo' : 'img/ashutosh.jpg','occupation': 'journalist', 'education' : 'btech', 'constituency' : 'Delhi North', 'state' : 'Delhi'},
+			 {'name' : 'Ashutosh', 'snippet' : 'Lorem Ipsum Lorem Ipsum Lorem Ipsum', 'photo' : 'img/ashutosh.jpg','occupation': 'journalist', 'education' : 'btech', 'constituency' : 'Delhi North', 'state' : 'Delhi'},
+			 {'name' : 'Ashutosh', 'snippet' : 'Lorem Ipsum Lorem Ipsum Lorem Ipsum', 'photo' : 'img/ashutosh.jpg','occupation': 'journalist', 'education' : 'btech', 'constituency' : 'Delhi North', 'state' : 'Delhi'}
+			 ];
+
+
 loksabhaApp.controller('LoksabhaCtrl', function ($scope) {
-  $scope.candidates = [
-		       {'name': 'Nexus S',
-		    'snippet': 'Fast just got faster with Nexus S.'},
-		   {'name': 'Motorola XOOM™ with Wi-Fi',
-		    'snippet': 'The Next, Next Generation tablet.'},
-		   {'name': 'MOTOROLA XOOM™',
-		    'snippet': 'The Next, Next Generation tablet.'}
-		   ];
+	$scope.candidates = GLOBAL_CANDIDATES;
     });
